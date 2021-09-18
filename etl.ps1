32c520b4d2bef48b48f9d4d3fda858e2bf0ff259
Joy George Kunjikkuru <joymon@gmail.com>
2021-09-18 17:32:46 -0400

    Added more fileds to repos

--- etl.ps1
+++ etl.ps1
@@ -1,5 +1,5 @@
 gh api user/orgs --paginate --jq '. | map({name: .login})' > .\data\organizations.json
-gh api user/repos --paginate --jq '. | map({created_at: .created_at, name: .name, organization: .owner.login,license: .license.name, archived: .archived, fork: .fork, forks_count: .forks_count,open_issues_count: .open_issues_count,stars_count: .stargazers_count, watchers_count: .watchers_count,size_kb:.size,has_pages: .has_pages, last_pushed_at: .pushed_at})' > ./data/repositories.json
+gh api user/repos --paginate --jq '. | map({created_at: .created_at, name: .name,html_url: .html_url, organization: .owner.login,organization_html_url: .owner.html_url, license: .license.name, archived: .archived, fork: .fork, forks_count: .forks_count,open_issues_count: .open_issues_count,stars_count: .stargazers_count, watchers_count: .watchers_count,size_kb:.size,has_pages: .has_pages, last_pushed_at: .pushed_at})' > ./data/repositories.json
 
 ################# Get Issues #############################
 # Get authenticated user issues 
