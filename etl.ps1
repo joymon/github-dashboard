89648ae66f9d5f58f72238b7b634807e05177175
Joy George Kunjikkuru <joymon@gmail.com>
2021-09-18 12:51:06 -0400

    Updates to repositories

--- etl.ps1
+++ etl.ps1
@@ -1,5 +1,5 @@
 gh api user/orgs --jq '. | map({name: .login})' > .\data\organizations.json
 
-gh api user/repos --jq '. | map({created_at: .created_at, name: .name, organization: .owner.login,archived: .archived, fork: .fork, forks_count: .forks_count,open_issues_count: .open_issues_count,stars_count: .stargazers_count, watchers_count: .watchers_count,size:.size})' > ./data/repositories.json
+gh api user/repos --paginate --jq '. | map({created_at: .created_at, name: .name, organization: .owner.login,archived: .archived, fork: .fork, forks_count: .forks_count,open_issues_count: .open_issues_count,stars_count: .stargazers_count, watchers_count: .watchers_count,size:.size})' > ./data/repositories.json
 gh api orgs/mind-maps/issues --jq '. | map({state: .state,created_at: .created_at, repository: .repository.name, organization: .repository.owner.login})' > .\data\issues.json
 #gh api orgs/mind-maps/issues --jq '. | map({state: .state,created_at: .created_at})'
