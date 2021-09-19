17bca605f331e2e4c59d430808d44428e8f3e389
Joy George Kunjikkuru <joymon@gmail.com>
2021-09-18 22:04:20 -0400

    fixed #3

--- etl.ps1
+++ etl.ps1
@@ -2,7 +2,7 @@ gh auth status
 "start: etl.ps1"
 gh api user/orgs --paginate --jq '. | map({name: .login})' > .\data\organizations.json
 "Pulled organizations data"
-gh api user/repos --paginate --jq '. | map({created_at: .created_at, name: .name,html_url: .html_url, organization: .owner.login,organization_html_url: .owner.html_url, license: .license.name, archived: .archived, fork: .fork, forks_count: .forks_count,open_issues_count: .open_issues_count,stars_count: .stargazers_count, watchers_count: .watchers_count,size_kb:.size,has_pages: .has_pages, last_pushed_at: .pushed_at,homepage: .homepage})' > ./data/repositories.json
+gh api user/repos --paginate --jq '. | map({created_at: .created_at, name: .name,html_url: .html_url, organization: .owner.login,organization_html_url: .owner.html_url, license: .license.name,is_private: .private, archived: .archived, fork: .fork, forks_count: .forks_count,open_issues_count: .open_issues_count,stars_count: .stargazers_count, watchers_count: .watchers_count,size_kb:.size,has_pages: .has_pages, last_pushed_at: .pushed_at,homepage: .homepage})' > ./data/repositories.json
 "Pulled repositories data"
 ################# Get Issues #############################
 # Get authenticated user issues 
